8b338c43d91975e4c26ed6deb973e0a4baef1e2a
Khouloud maamouri <khouloud.maamouri97@gmail.com>
2022-01-22 21:05:23 +0100

    change patch to post

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,6 @@ module.exports = (app, pathApi) => {
   const userControl = require("../controllers/userControl");
   app.get(pathApi + "/users", userControl.listUser_get);
   app.delete(pathApi + "/users/:id", userControl.removeUser_delete);
-  app.patch(pathApi + "/users/:id", userControl.updateUser_patch);
+  app.post(pathApi + "/users/:id", userControl.updateUser_patch);
   app.get(pathApi + "/users/:id", userControl.getUserById_get);
 };
